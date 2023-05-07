127eaaab03ca3f681aff08404ba11e0890af4c80
Pranav Shekar <pshekar@purdue.edu>
2023-05-06 21:01:41 -0400

    Add benchmark prints, fix cost function

--- neural_net.py
+++ neural_net.py
@@ -14,10 +14,7 @@ file_name = 'hw3_wine.csv'
 def main(regularization, net_shape, k_folds):
     with open(f'{cur_path}/datasets/{file_name}', newline='') as csvfile:
         data = csvfile.read().splitlines()
-        # testing_categories = deepcopy(categories)
-        # testing_accuracy = 0
-        # testing_precision = 0
-        # testing_recall = 0
+        # exclude attributes
         data = data[1:]
         data_set = []
         for row in data:
@@ -36,7 +33,6 @@ def main(regularization, net_shape, k_folds):
                         arr[i] = float(arr[i])
             data_set.append(np.array(arr))
         data_set = np.array(data_set)
-        # normalize/regularize data?
         # stratified cross validation
         weights = set_weights(net_shape)
         # for i in range(k_folds):
@@ -67,8 +63,6 @@ def main(regularization, net_shape, k_folds):
         elif file_name == 'hw3_wine.csv':
             training_set = data_set[:, 1:]
             training_set = normalize(training_set)
-            # training_set = training_set / training_set.max(axis=0)
-        test(weights, training_set, expected_outputs)
         final_weights = back_propogate(weights, training_set, expected_outputs, net_shape, regularization)
         test(final_weights, training_set, expected_outputs)
 
@@ -146,18 +140,28 @@ def set_weights(net_shape):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def forward_propogate(weights, inputs):
+def forward_propogate(weights, inputs, benchmark=False):
     outputs = []
     output = np.insert(inputs, 0, 1)
+    if benchmark:
+        print(f'a1: {output}')
     outputs.append(output)
     for i in range(len(weights) - 1):
         output = np.matmul(weights[i], output)
+        if benchmark:
+            print(f'z{i+2}: {output}')
         output = sigmoid(output)
         output = np.insert(output, 0, 1)
+        if benchmark:
+            print(f'a{i+2}: {output}')
         outputs.append(output)
 
     output = np.matmul(weights[-1], output)
+    if benchmark:
+       print(f'z{i+3}: {output}')
     output = sigmoid(output)
+    if benchmark:
+        print(f'a{i+3}: {output}')
     outputs.append(output)
     return output, outputs
 
@@ -171,13 +175,15 @@ def regularize(weights, inputs, regularization):
     square_weights = 0
     for matrix in weights:
         for row in matrix:
-            for i in range(len(row)-1):
+            for i in range(len(row)):
+                if i == 0:
+                    continue
                 square_weights += (row[i] * row[i])
 
     regularized = (regularization / (2 * len(inputs))) * square_weights
     return regularized
 
-def back_propogate(weights, inputs, expected_outputs, net_shape, regularization):
+def back_propogate(weights, inputs, expected_outputs, net_shape, regularization, benchmark=False):
     # initialize big ol D
     weights_copy = deepcopy(weights)
     gradients = []
@@ -187,7 +193,7 @@ def back_propogate(weights, inputs, expected_outputs, net_shape, regularization)
     for _ in range(1000):
         for k in range(len(inputs)):
             deltas = []
-            output, outputs = forward_propogate(weights, inputs[k])
+            output, outputs = forward_propogate(weights, inputs[k], benchmark)
             # print('back')
             output_delta = output - expected_outputs[k]
             # print(f'output_delta: {np.array([np.array(output_delta)])=}')
@@ -229,14 +235,23 @@ def benchmark1(regularization, net_shape):
     expected_outputs = [[0.9000], [.23000]]
     # weights = set_weights(inputs, net_shape)
     weights = [[[0.40000,  0.10000], [0.30000,  0.20000  ]], [[0.70000,  0.50000,  0.60000]]]
-    output = back_propogate(weights, inputs, expected_outputs, net_shape, regularization)
+    # output = back_propogate(weights, inputs, expected_outputs, net_shape, regularization, True)
     # cost = cost_fn(expected_output1, output1)
-    # output2, _ = forward_propogate(weights, input2)
-    # cost += cost_fn(expected_output2, output2)
-    # cost = cost / 2
-    # regularized = regularize(weights, input1, regularization)
-    # regularized_cost = cost + regularized
-    # print(f'{regularized_cost=}')
+    costs = 0
+    for i in range(len(inputs)):
+        print(f'Forward propagate of instance {i+1}')
+        output, _ = forward_propogate(weights, inputs[i], True)
+        print(f'f(x) = {output}')
+        print(f'Predicted output for instance {i+1}: {output}')
+        print(f'Expected output for instance {i+1}: {expected_outputs[i]}')
+        cost = cost_fn(expected_outputs[i], output)
+        print(f'Cost, J, associated with instance {i+1}: {cost}')
+        costs += cost
+        print()
+    costs = costs / len(inputs)
+    regularized = regularize(weights, inputs, regularization)
+    regularized_cost = costs + regularized
+    print(f'Final (regularized) cost, J, based on the complete training set: {regularized_cost}')
 
 def benchmark2(regularization, net_shape):
     inputs = [[0.32000, 0.68000], [0.83000, 0.02000]]
@@ -244,24 +259,33 @@ def benchmark2(regularization, net_shape):
     weights = [[[0.42000, 0.15000, 0.40000], [0.72000, 0.10000, 0.54000], [0.01000, 0.19000, 0.42000],	[0.30000, 0.35000, 0.68000]], 
                [[0.21000, 0.67000, 0.14000, 0.96000, 0.87000], [0.87000, 0.42000, 0.20000, 0.32000, 0.89000], [0.03000, 0.56000, 0.80000, 0.69000, 0.09000]],
                [[0.04000, 0.87000, 0.42000, 0.53000], [0.17000, 0.10000, 0.95000, 0.69000]]]
-    output = back_propogate(weights, inputs, expected_outputs, net_shape, regularization)
-    # cost = cost_fn(expected_output1, output1)
-    # output2, _ = forward_propogate(weights, input2)
-    # cost += cost_fn(expected_output2, output2)
-    # cost = cost / 2
-    # regularized = regularize(weights, input1, regularization)
-    # regularized_cost = cost + regularized
-    # print(f'{regularized_cost=}')
+    # output = back_propogate(weights, inputs, expected_outputs, net_shape, regularization, True)
+    costs = 0
+    for i in range(len(inputs)):
+        print(f'Forward propagate of instance {i+1}')
+        output, _ = forward_propogate(weights, inputs[i], True)
+        print(f'f(x) = {output}')
+        print(f'Predicted output for instance {i+1}: {output}')
+        print(f'Expected output for instance {i+1}: {expected_outputs[i]}')
+        cost = cost_fn(expected_outputs[i], output)
+        print(f'Cost, J, associated with instance {i+1}: {cost}')
+        costs += cost
+        print()
+    costs = costs / len(inputs)
+    regularized = regularize(weights, inputs, regularization)
+    regularized_cost = costs + regularized
+    print(f'Final (regularized) cost, J, based on the complete training set: {regularized_cost}')
 
 
 if __name__ == '__main__':
-    regularization = 0
+    # regularization = 0
     # net_shape = [1, 2, 1]
     # benchmark1(regularization, net_shape)
     # regularization = 0.25
     # net_shape = [2, 4, 3, 2]
     # benchmark2(regularization, net_shape)
     k_folds = 10
+    regularization = 0
     # net_shape = [16, 10, 10, 10, 2]
     net_shape = [13, 10, 10, 10, 3]
     main(regularization, net_shape, k_folds)
